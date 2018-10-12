e084d215c33e57f2d44d5586160f91918c821afc
JFortey <j-fortey@live.co.uk>
2018-10-12 21:44:09 +0100

    Added pause function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,13 +6,17 @@ using UnityStandardAssets.CrossPlatformInput;
 public class GameManager : MonoBehaviour
 {
     public bool recording = true;
+    private float fixedDeltaTime;
 
     void Start()
     {
         PlayerPrefsManager.UnlockLevel(2);
         print(PlayerPrefsManager.IsLevelUnlocked(1));
         print(PlayerPrefsManager.IsLevelUnlocked(2));
+        fixedDeltaTime = Time.fixedDeltaTime;
+        print(fixedDeltaTime);
     }
+   
     // Update is called once per frame
     void Update ()
     {
@@ -24,5 +28,27 @@ public class GameManager : MonoBehaviour
         {
             recording = true;
         }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PauseGame();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResumeGame();
+        }
 	}
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = 0;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = fixedDeltaTime;
+    }
 }
